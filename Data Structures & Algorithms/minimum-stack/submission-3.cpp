0a4c834fa546f850e96bb53f6ba6cec32d8dcdce
bigboss5696 <v.amuoi1996@gmail.com>
2026-07-28 15:31:37 +0700

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,25 @@
+class MinStack {
+   public:
+    MinStack() {}
+
+    std::stack<int> mainStack;
+    std::stack<int> minStack;
+
+    void push(int val) {
+        mainStack.push(val);
+        if (minStack.empty() || val < minStack.top()) {
+            minStack.push(val);
+        } else {
+            minStack.push(minStack.top());
+        }
+    }
+
+    void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+
+    int top() { return mainStack.top(); }
+
+    int getMin() { return minStack.top(); }
+};
